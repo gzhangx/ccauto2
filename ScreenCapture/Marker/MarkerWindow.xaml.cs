e85fb5fc8e381d35a5576b0883d7712eb63be79c
Gang Zhang <gzhangx@hotmail.com>
2024-01-04 10:15:38 -0500

    sel

--- ScreenCapture/Marker/MarkerWindow.xaml.cs
+++ ScreenCapture/Marker/MarkerWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Windows.Storage.Streams;
+using Point = System.Windows.Point;
 
 namespace ccauto.Marker
 {
@@ -46,6 +47,81 @@ namespace ccauto.Marker
                     canvImg.Source = bitmapimage;
                 }
             }
+        }
+
+
+
+        static EasyRect PointsToRect(Point p1, Point p2)
+        {
+            EasyRect r = new EasyRect();
+            r.X = (int)p1.X;
+            r.Width = (int)(p2.X - p1.X);
+            if (r.Width < 0)
+            {
+                r.X = (int)p2.X;
+                r.Width = -r.Width;
+            }
+            r.Y = (int)p1.Y;
+            r.Height = (int)(p2.Y - p1.Y);
+            if (r.Height < 0)
+            {
+                r.Y = (int)p2.Y;
+                r.Height = -r.Height;
+            }
+            //Console.WriteLine("X=" + r.X.ToString("0.0") + " y=" + r.Y.ToString("0.0")+ " width="+r.Width.ToString("0") + " h="+r.Height.ToString("0"));
+            return r;
+        }
+
+        Point mouseDownP;
+        Point mouseUpP;
+        EasyRect curSelRect = null;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            canvImg.MouseLeftButtonDown += (s, mouseE) =>
+            {
+                Point p = mouseE.GetPosition(canvImg);
+                mouseDownP = p;
+                mouseUpP.X = -1;
+                canvImg.CaptureMouse();
+                mouseE.Handled = true;
+                mouseDspRect.Width = 0; mouseDspRect.Height = 0;
+                mouseDspRect.Visibility = Visibility.Visible;
+            };
+            canvImg.MouseLeftButtonUp += (s, mouseE) =>
+            {
+                Point p = mouseE.GetPosition(canvImg);
+                mouseUpP = p;
+                Console.WriteLine("release mouse cap");
+                canvImg.ReleaseMouseCapture();
+                mouseE.Handled = true;
+            };
+            canvImg.MouseRightButtonUp += (s, mouseE) =>
+            {
+                mouseUpP.X = -1;
+                mouseDownP.X -= 1;
+                mouseDspRect.Visibility = Visibility.Collapsed;
+                curSelRect = null;
+            };
+
+            canvImg.MouseMove += (s, mouseE) =>
+            {
+                if (mouseDownP.X < 0) return;
+                if (mouseUpP.X >= 0) return;
+                Point p = mouseE.GetPosition(canvImg);
+                var r = PointsToRect(mouseDownP, p);
+                if (r.Width <= 0) return;
+                if (r.Height <= 0) return;
+                curSelRect = r;
+                Canvas.SetLeft(mouseDspRect, r.X);
+                Canvas.SetTop(mouseDspRect, r.Y);
+                mouseDspRect.Width = r.Width;
+                mouseDspRect.Height = r.Height;
+                var brush = new System.Windows.Media.SolidColorBrush();
+                brush.Color = System.Windows.Media.Colors.Red;
+                brush.Opacity = 0.5;
+                mouseE.Handled = true;
+            };
         }
     }
 }
